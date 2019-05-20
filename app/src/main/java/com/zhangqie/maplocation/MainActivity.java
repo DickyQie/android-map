8aa6074965b159851ec46caaec133dd0cd07b3a0
张切 <zhangqie@hzrht.com>
2019-05-20 15:48:15 +0800

    更新高德SDK，使用依赖的方式实现定位，更新Android的sdk

---
+++ app/src/main/java/com/zhangqie/maplocation/MainActivity.java
@@ -0,0 +1,163 @@
+package com.zhangqie.maplocation;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+/***
+ * 高德定位
+ */
+public class MainActivity extends AppCompatActivity implements AMapLocationListener {
+
+    private static final int MY_PERMISSIONS_REQUEST_CALL_LOCATION = 1;
+    public AMapLocationClient mlocationClient;
+    public AMapLocationClientOption mLocationOption = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //检查版本是否大于M
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_CALL_LOCATION);
+            } else {
+                //"权限已申请";
+                showLocation();
+            }
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_CALL_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //"权限已申请"
+                showLocation();
+            } else {
+                showToast("权限已拒绝,不能定位");
+
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    // TODO:
+    private void showLocation() {
+        try {
+            mlocationClient = new AMapLocationClient(this);
+            mLocationOption = new AMapLocationClientOption();
+            mlocationClient.setLocationListener(this);
+            //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            mLocationOption.setInterval(5000);
+            //设置定位参数
+            mlocationClient.setLocationOption(mLocationOption);
+            //启动定位
+            mlocationClient.startLocation();
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+        try {
+            if (amapLocation != null) {
+                if (amapLocation.getErrorCode() == 0) {
+                    //定位成功回调信息，设置相关消息
+
+                    //获取当前定位结果来源，如网络定位结果，详见定位类型表
+                    Log.i("定位类型", amapLocation.getLocationType() + "");
+                    Log.i("获取纬度", amapLocation.getLatitude() + "");
+                    Log.i("获取经度", amapLocation.getLongitude() + "");
+                    Log.i("获取精度信息", amapLocation.getAccuracy() + "");
+
+                    //如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
+                    Log.i("地址", amapLocation.getAddress());
+                    Log.i("国家信息", amapLocation.getCountry());
+                    Log.i("省信息", amapLocation.getProvince());
+                    Log.i("城市信息", amapLocation.getCity());
+                    Log.i("城区信息", amapLocation.getDistrict());
+                    Log.i("街道信息", amapLocation.getStreet());
+                    Log.i("街道门牌号信息", amapLocation.getStreetNum());
+                    Log.i("城市编码", amapLocation.getCityCode());
+                    Log.i("地区编码", amapLocation.getAdCode());
+                    Log.i("获取当前定位点的AOI信息", amapLocation.getAoiName());
+                    Log.i("获取当前室内定位的建筑物Id", amapLocation.getBuildingId());
+                    Log.i("获取当前室内定位的楼层", amapLocation.getFloor());
+                    Log.i("获取GPS的当前状态", amapLocation.getGpsAccuracyStatus() + "");
+
+                    //获取定位时间
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    Date date = new Date(amapLocation.getTime());
+
+                    Log.i("获取定位时间", df.format(date));
+
+
+                    // 停止定位
+                    mlocationClient.stopLocation();
+                } else {
+                    //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                    Log.e("AmapError", "location Error, ErrCode:"
+                            + amapLocation.getErrorCode() + ", errInfo:"
+                            + amapLocation.getErrorInfo());
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // 停止定位
+        if (null != mlocationClient) {
+            mlocationClient.stopLocation();
+        }
+    }
+
+    /**
+     * 销毁定位
+     */
+    private void destroyLocation() {
+        if (null != mlocationClient) {
+            /**
+             * 如果AMapLocationClient是在当前Activity实例化的，
+             * 在Activity的onDestroy中一定要执行AMapLocationClient的onDestroy
+             */
+            mlocationClient.onDestroy();
+            mlocationClient = null;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        destroyLocation();
+        super.onDestroy();
+    }
+
+    private void showToast(String string) {
+        Toast.makeText(MainActivity.this, string, Toast.LENGTH_LONG).show();
+    }
+
+}
